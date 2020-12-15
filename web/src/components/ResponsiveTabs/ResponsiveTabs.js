c6ec27d1c4bf380aec2a855624942dd28e9ccb0a
Ben Galloway <ben@neu.studio>
2020-12-15 15:59:15 +0000

    Make tabs accordion rather than menu on small screens

--- web/src/components/ResponsiveTabs/ResponsiveTabs.js
+++ web/src/components/ResponsiveTabs/ResponsiveTabs.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import styled, { useTheme } from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { navigate, routes } from "@redwoodjs/router";
 import { EuiIcon, EuiTabs, EuiTab } from "@elastic/eui";
 import useWindowWidth from "src/utils/useWindowWidth";
@@ -10,57 +9,78 @@ const ResponsiveTabs = ({ tabs, selectedTabId, topLevelRoute }) => {
   const width = useWindowWidth();
   const isMobile = width <= theme.archive.breakpoints.m;
 
-  const [expanded, setExpanded] = useState(false);
-  const toggleExpansion = () => setExpanded((prev) => !prev);
   const handleTabClick = (tab) => () => {
     navigate(routes[topLevelRoute]({ tab: tab.id }));
-    setExpanded(false);
   };
 
+  const [selectedTab] = tabs.filter((tab) => tab.id === selectedTabId);
+
   return (
-    <StyledTabs>
-      {tabs.map((tab) => {
-        const isSelected = tab.id === selectedTabId;
-        return (
-          <TabWrapper
-            key={tab.id}
-            animate={{
-              height: !isMobile || isSelected || expanded ? "auto" : 0,
-            }}
-            transition={{
-              duration: 0.2,
-            }}
+    <>
+      <StyledTabs>
+        {tabs.map((tab) => {
+          const isSelected = tab.id === selectedTabId;
+          return (
+            <TabWrapper key={tab.id}>
+              <StyledTab
+                {...(tab.href && { href: tab.href, target: "_blank" })}
+                onClick={handleTabClick(tab)}
+                isSelected={isSelected}
+                disabled={tab.disabled}
+              >
+                <TabInner>
+                  <span>
+                    <TabIcon type={tab.icon} />
+                    {tab.name}
+                  </span>
+                  {isMobile && <EuiIcon type={isSelected ? "" : "arrowDown"} />}
+                </TabInner>
+              </StyledTab>
+              {isMobile && (
+                <Content
+                  initial={{
+                    height: 0,
+                  }}
+                  animate={{
+                    height: isSelected ? "auto" : 0,
+                  }}
+                  transition={{
+                    duration: 0.3,
+                  }}
+                >
+                  <ContentWrapper>{tab.content}</ContentWrapper>
+                </Content>
+              )}
+            </TabWrapper>
+          );
+        })}
+      </StyledTabs>
+      {!isMobile && (
+        <AnimatePresence exitBeforeEnter>
+          <Content
+            key={selectedTabId}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
           >
-            <StyledTab
-              {...(tab.href && { href: tab.href, target: "_blank" })}
-              onClick={isSelected ? toggleExpansion : handleTabClick(tab)}
-              isSelected={isSelected}
-              disabled={tab.disabled}
-            >
-              <TabInner>
-                <span>
-                  <TabIcon type={tab.icon} />
-                  {tab.name}
-                </span>
-                {isMobile && isSelected && (
-                  <EuiIcon type={expanded ? "arrowUp" : "arrowDown"} />
-                )}
-              </TabInner>
-            </StyledTab>
-          </TabWrapper>
-        );
-      })}
-    </StyledTabs>
+            <ContentWrapper>{selectedTab.content}</ContentWrapper>
+          </Content>
+        </AnimatePresence>
+      )}
+    </>
   );
 };
 
 const StyledTabs = styled(EuiTabs)`
+  margin: ${({ theme }) => `${theme.euiSizeS} ${theme.euiSizeM} 0`};
   @media (max-width: ${({ theme }) => theme.euiBreakpoints.m}) {
+    margin: 0;
     flex-direction: column;
   }
 `;
 
-const TabWrapper = styled(motion.div)`
+const TabWrapper = styled.div`
   overflow: hidden;
 `;
 
@@ -80,6 +100,12 @@ const TabInner = styled.div`
 
 const TabIcon = styled(EuiIcon)`
   margin-right: ${({ theme }) => theme.euiSizeS};
+`;
+
+const Content = styled(motion.div)``;
+
+const ContentWrapper = styled.div`
+  padding: ${({ theme }) => `${theme.euiSizeXL} ${theme.euiSizeL}`};
 `;
 
 export default ResponsiveTabs;
